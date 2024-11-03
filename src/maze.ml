1c94fbd59104cd45f181b740313bf6d1214c8ae8
SakuraZ1 <129630244+SakuraZ1@users.noreply.github.com>
2024-11-03 15:55:34 -0500

    Update maze.ml

--- src/maze.ml
+++ src/maze.ml
@@ -1,5 +1,5 @@
 module type MAZE = sig
-  type cell = 0 | 1  
+  type cell = 0 | 1  '''// 0 stands for Wall and 1 stands for Path'''
   type maze = cell list list
 
   val create : int -> int -> maze
