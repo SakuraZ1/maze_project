39270a5f061b4dee4b29bae9e78445bd2c249727
SakuraZ1 <129630244+SakuraZ1@users.noreply.github.com>
2024-11-04 14:11:36 -0500

    Update maze.mli

--- src/maze.mli
+++ src/maze.mli
@@ -11,3 +11,28 @@ module type MAZE = sig
 end
 
 
+module type MAZE = sig
+  type direction = North | South | East | West
+
+  type cell = {
+    x : int;
+    y : int;
+    walls : (direction * bool) list;
+  }
+
+  type maze = {
+    width : int;
+    height : int;
+    grid : cell array array;
+  }
+
+  val create : int -> int -> maze
+  val initialize_cells : maze -> unit
+  val display : maze -> unit
+  val get_cell : maze -> int -> int -> cell
+  val set_cell : maze -> cell -> unit
+  val in_bounds : maze -> int -> int -> bool
+  val get_neighbors : maze -> cell -> cell list
+  val remove_wall : maze -> int -> int -> int -> int -> unit
+  val get_passable_neighbors : maze -> cell -> cell list
+end
