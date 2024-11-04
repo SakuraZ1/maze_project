fe401d818c5e3871161308cdee5f915a465ad51a
SakuraZ1 <129630244+SakuraZ1@users.noreply.github.com>
2024-11-04 14:07:51 -0500

    Update maze.mli

--- src/maze.mli
+++ src/maze.mli
@@ -1,1 +1,13 @@
+module type MAZE = sig
+  type cell = Wall | Path   
+  type maze = {
+    width : int;
+    height : int;
+    grid : cell array array;
+    }
+
+  val create : int -> int -> maze
+  val display : maze -> unit
+end
+
 
