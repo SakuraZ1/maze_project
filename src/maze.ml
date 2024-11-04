e97056a9a75c07cbaa23ef509ac41809a1fc3d2e
SakuraZ1 <129630244+SakuraZ1@users.noreply.github.com>
2024-11-04 13:49:45 -0500

    Update maze.ml

--- src/maze.ml
+++ src/maze.ml
@@ -1,9 +1,39 @@
 module type MAZE = sig
   type cell = Wall | Path   
-  type maze = cell list list
+  type maze = {
+    width : int;
+    height : int;
+    grid : cell array array;
+    }
 
   val create : int -> int -> maze
   val display : maze -> unit
 end
+
+
+module Maze : MAZE = struct
+  type cell = Wall | Path
+
+  type maze = {
+    width : int;
+    height : int;
+    grid : cell array array;
+  }
+
+  let create width height =
+    let grid = Array.make_matrix width height Wall 
+    in
+    { width; height; grid }
+
+
+  let display maze =
+    for y = 0 to maze.height - 1 do
+      for x = 0 to maze.width - 1 do
+        match maze.grid.(x).(y) with
+        | Wall -> print_string "#"
+        | Path -> print_string " "
+      done;
+      print_newline ()
+    done
 
 
