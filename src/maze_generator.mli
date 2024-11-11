dd86f31af97b7f69bf424466affca878b4b38db1
SakuraZ1 <129630244+SakuraZ1@users.noreply.github.com>
2024-11-11 09:56:18 -0500

    Update maze_generator.mli

--- src/maze_generator.mli
+++ src/maze_generator.mli
@@ -1,1 +1,18 @@
 
+module type MAZE_GENERATOR = sig
+  (* [generate maze] generates a maze using the implemented algorithm, modifying [maze] in place. *)
+  val generate : Maze.maze -> unit
+end
+
+(* Functor to create a maze generator. *)
+module MakeGenerator : functor (M : MAZE_GENERATOR) -> MAZE_GENERATOR
+
+(* Recursive Backtracking Maze Generator *)
+module RecursiveBacktrackerGenerator : MAZE_GENERATOR
+
+(* Prim's Algorithm Maze Generator *)
+module PrimGenerator : MAZE_GENERATOR
+
+(* Kruskal's Algorithm Maze Generator *)
+module KruskalGenerator : MAZE_GENERATOR
+
