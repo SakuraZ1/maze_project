8630a02995172e4f06212131529892d0c2a4540b
SakuraZ1 <129630244+SakuraZ1@users.noreply.github.com>
2024-11-11 10:10:58 -0500

    Update maze_solver.mli

--- src/maze_solver.mli
+++ src/maze_solver.mli
@@ -1,1 +1,16 @@
 
+module type MAZE_SOLVER = sig
+  (* [solve maze] finds a path from the start to the end of [maze],
+      returning a list of coordinates representing the solution path. *)
+  val solve : Maze.maze -> (int * int) list
+end
+
+(* Functor to create a maze solver. *)
+module MakeSolver : functor (M : MAZE_SOLVER) -> MAZE_SOLVER
+
+(* BFS Maze Solver *)
+module BFSSolver : MAZE_SOLVER
+
+(* A\* Search Algorithm Maze Solver *)
+module AStarSolver : MAZE_SOLVER
+
