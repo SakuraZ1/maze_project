85376e835b2178445e302321e125b5b49f8d6b6d
SakuraZ1 <yf610812@gmail.com>
2024-12-06 16:06:01 -0500

    change solver_test

--- test/maze_solver_test.ml
+++ test/maze_solver_test.ml
@@ -1,4 +1,4 @@
-open OUnit2
+(*open OUnit2
 open Maze
 open Maze_solver
 
@@ -16,6 +16,52 @@ let test_astar_solver _ =
 
 let suite =
   "Maze Solver Tests" >::: [
+    "test_bfs_solver" >:: test_bfs_solver;
+    "test_astar_solver" >:: test_astar_solver;
+  ]
+
+let () =
+  run_test_tt_main suite
+*)
+
+open OUnit2
+open Maze
+open Maze_solver
+open Maze_generator
+
+let test_bfs_solver _ =
+  (* Generate a simple connected maze using the RecursiveBacktrackerGenerator *)
+  let maze = Maze.create 5 5 in
+  let module Generator = RecursiveBacktrackerGenerator in
+  let maze = Generator.generate maze in
+  let solution = BFSSolver.solve maze in
+
+  (* Assert that the solution is non-empty and ends at the goal (bottom-right corner) *)
+  assert_bool "Solution is non-empty" (List.length solution > 0);
+  match List.rev solution with
+  | [] -> assert_failure "Solution should not be empty"
+  | end_cell :: _ -> 
+      let (ex, ey) = end_cell in
+      assert_equal (maze.width - 1, maze.height - 1) (ex, ey)
+
+let test_astar_solver _ =
+  (* Generate a simple connected maze using the RecursiveBacktrackerGenerator *)
+  let maze = Maze.create 5 5 in
+  let module Generator = RecursiveBacktrackerGenerator in
+  let maze = Generator.generate maze in
+  let solution = AStarSolver.solve maze in
+
+  (* Assert that the solution is non-empty and ends at the goal (bottom-right corner) *)
+  assert_bool "Solution is non-empty" (List.length solution > 0);
+  match List.rev solution with
+  | [] -> assert_failure "Solution should not be empty"
+  | end_cell :: _ ->
+      let (ex, ey) = end_cell in
+      assert_equal (maze.width - 1, maze.height - 1) (ex, ey)
+
+let suite =
+  "Maze Solver Tests" >:::
+  [
     "test_bfs_solver" >:: test_bfs_solver;
     "test_astar_solver" >:: test_astar_solver;
   ]
--- test/maze_test.ml
+++ test/maze_test.ml
@@ -27,6 +27,8 @@ let suite =
 let () =
   run_test_tt_main suite
 *)
+
+
  open OUnit2
   open Maze
   open Cell
