239fdbf69a14e6469ebfd436756791f1d93e3562
SakuraZ1 <yf610812@gmail.com>
2024-12-06 14:32:05 -0500

    fixed bugs in tests

--- test/cell_test.ml
+++ test/cell_test.ml
@@ -1,22 +1,17 @@
+(* test/cell_test.ml *)
+
 open OUnit2
-open Core
 open Cell
 
 let test_create_cell _ =
-  let cell = Cell.create 1 2 in
-  assert_equal ~msg:"Cell X coordinate" 1 cell.x;
-  assert_equal ~msg:"Cell Y coordinate" 2 cell.y;
-  assert_equal ~msg:"Cell walls initialized correctly" [(North, true); (South, true); (East, true); (West, true)] cell.walls
-
-let test_set_wall _ =
-  let cell = Cell.create 0 0 in
-  let cell = Cell.set_wall cell North false in
-  assert_equal ~msg:"North wall should be removed" false (List.assoc North cell.walls)
+  let cell = { x = 1; y = 2; walls = [(North, true); (South, true)] } in
+  assert_equal cell.x 1;
+  assert_equal cell.y 2;
+  assert_equal (List.length cell.walls) 2
 
 let suite =
-  "Cell Tests" >::: [
+  "Cell Test Suite" >::: [
     "test_create_cell" >:: test_create_cell;
-    "test_set_wall" >:: test_set_wall;
   ]
 
 let () =
--- test/maze_test.ml
+++ test/maze_test.ml
@@ -1,23 +1,29 @@
+(* test/maze_test.ml *)
+
 open OUnit2
 open Maze
 
 let test_create_maze _ =
   let maze = Maze.create 3 3 in
-  assert_equal ~msg:"Maze width" 3 (Maze.get_width maze);
-  assert_equal ~msg:"Maze height" 3 (Maze.get_height maze);
-  assert_bool "Grid is initialized correctly" (Array.length (Maze.get_grid maze) = 3)
+  assert_equal (Maze.get_width maze) 3;
+  assert_equal (Maze.get_height maze) 3;
+  assert_bool "Grid is initialized correctly" (List.length (Maze.get_grid maze) = 3);
+  assert_bool "Each row has correct number of cells" (
+    List.for_all (fun row -> List.length row = 3) (Maze.get_grid maze)
+  )
 
 let test_get_cell _ =
   let maze = Maze.create 3 3 in
   let cell = Maze.get_cell maze 1 1 in
-  assert_equal ~msg:"Cell X coordinate" 1 cell.x;
-  assert_equal ~msg:"Cell Y coordinate" 1 cell.y
+  assert_equal cell.x 1;
+  assert_equal cell.y 1
 
 let suite =
-  "Maze Tests" >::: [
+  "Maze Test Suite" >::: [
     "test_create_maze" >:: test_create_maze;
     "test_get_cell" >:: test_get_cell;
   ]
 
 let () =
   run_test_tt_main suite
+
